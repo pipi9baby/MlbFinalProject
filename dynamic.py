8dcdd3d2f3295b96e51713628a233ca825b264a9
pipi9baby <pipi9baby@gmail.com>
2018-12-06 18:57:36 +0800

    第一部分完成，程式碼還沒修

--- dynamic.py
+++ dynamic.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
+
 #動態規劃法，輸入兩個string回傳一個int
-def dynamicString(self,str1, str2):
+def dynamicString(str1, str2):
 	test_list=[ [999] * (len(str2)+1) for i in range(len(str1)+1) ]
 
 	#初始化邊界的數值
@@ -17,3 +19,92 @@ def dynamicString(self,str1, str2):
 			else:
 				test_list[i][j] = 1 + min([test_list[i-1][j-1], test_list[i-1][j], test_list[i][j-1]])
 	return test_list[len(str1)][len(str2)]
+
+testFile = r"C:\Users\MINHAN\Desktop\signalp\test\NCEuk.nr.fasta"
+trainFile = r"C:\Users\MINHAN\Desktop\signalp\train\train.fasta"
+
+aminoNum = 35
+
+#讀取train data的資料
+fr = open(trainFile)
+train = fr.read()
+fr.close()
+trainLi = train.split(">")
+trainDataSeq = []
+trainY = []
+for eachData in trainLi:
+	if eachData == "":
+		continue
+	
+	tmpLi = eachData.split("\n")
+	trainDataSeq.append(tmpLi[1][:aminoNum])
+	if "S" in tmpLi[2]:
+		trainY.append(1)
+	else:
+		trainY.append(0)
+
+print("training資料已讀取")
+#讀取testDataSeq的資料
+fr = open(testFile)
+test = fr.read()
+fr.close()
+testLi = test.split(">")
+testDataSeq = []
+for eachData in testLi:
+	if eachData == "":
+		continue
+	
+	tmpLi = eachData.split("\n")
+	testDataSeq.append(tmpLi[1][:aminoNum])
+
+print("test資料已讀取")
+#取train data最像的20個
+seqSimLi = []
+for seq1 in trainDataSeq:
+	simDict = defaultdict(list)
+	for seq2 in trainDataSeq:
+		if seq1 == seq2:
+			continue
+		
+		returnInt = dynamicString(seq1, seq2)
+		simDict[returnInt].append(seq2)
+	
+	#找最大的20個
+	items = simDict.items()
+	items.sort()
+	limit = 0
+	for key,value in items:
+		for eachSeq in value:
+			index = trainDataSeq.index(value)
+			seqSimLi.append(trainY[index])
+			limit += 1
+			if(limit > 20):
+				break
+		if(limit > 20):
+			break
+
+print("KNN前20資料已讀取")
+#做trainData 的 one hot encoding
+aminoDict ={'G':0,'A':1,'V':2,'L':3,'I':4,'F':5,'W':6,'Y':7,'D':8,'H':9,'N':10,'E':11,'K':12,'Q':13,'M':14,'R':15,'S':16,'T':17,'C':18,'P':19}
+trainX = []
+for seq in trainDataSeq:
+	for amino in seq:
+		#做one hot encoding
+		tmpLi = [0] * 20
+		tmpLi[amino] = 1
+		trainX = trainX + tmpLi
+	#存最後20個像的
+	trainX = trainX + seqSimLi[:20]
+	seqSimLi = seqSimLi[20:]
+
+#做trainData 的 one hot encoding
+testX = []
+for seq in trainDataSeq:
+	for amino in seq:
+		#做one hot encoding
+		tmpLi = [0] * 20
+		tmpLi[amino] = 1
+		testX = testX + tmpLi
+
+print(testX, trainX, trainY)
+print("successful!")
